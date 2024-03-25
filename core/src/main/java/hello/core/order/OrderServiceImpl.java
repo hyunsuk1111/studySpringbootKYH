01451b287beedbc71a978aacb2773087a488df1a
hyunsuk1111 <epsxkf@gmail.com>
2024-03-25 17:27:54 +0900

    롬복 라이브러리[@RequiredArgsConstructor] 추가

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+//final 필드들을 모아서 생성자를 만든다음 의존성 주입해줌
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //인터페이스에만 의존하게한다.
@@ -14,12 +16,6 @@ public class OrderServiceImpl implements OrderService {
     //구현객체를 생성해서 주입해줘야한다!
     private final MemberRepository  memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
